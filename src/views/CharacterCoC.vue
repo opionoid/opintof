144fe61d5408c7f501b9fa2a14dd677a933015a2
Opion <kindledkindred@gmail.com>
2019-06-15 17:56:54 +0900

    add the randomized ability and the form of HP/MP/SAN point
    TODO
    The EDU ability in randomized ability is NOT based on the character's job. I have to create SelectJob.vue, and prop the job data as array of ability to the child page (CoCCharacter.vue)

--- src/views/CharacterCoC.vue
+++ src/views/CharacterCoC.vue
@@ -19,12 +19,16 @@
         span.status_name 知識
         span.status_value : {{ Knowledge }}
     v-layout(row wrap)
-      v-flex(xs3)
-        .status HP: {{ maxHP }}
-      v-flex(xs3)
-        .status MP: {{ maxMP }}
-      v-flex(xs3)
-        .status SAN: {{ SAN }}
+      v-flex.mr-4(xs3)
+        v-text-field(v-model="currentHP" label="HP")
+      v-flex.mr-4(xs3)
+        v-text-field(v-model="currentMP" label="MP")
+      v-flex.mr-4(xs3)
+        v-text-field(v-model="currentSAN" label="SAN")
+    v-layout(row wrap)
+      v-flex(xs3 v-for="ability in ability_list")
+        span.status_name {{ ability.name }}
+        span.status_value : {{ ability.value }}
 </template>
 
 <script>
@@ -87,26 +91,273 @@ export default {
     Knowledge: function() {
       return this.status_list[7].value * 5;
     },
-    maxHP: function() {
+    currentHP: function() {
       return Math.floor(
         (this.status_list[1].value + this.status_list[5].value) / 2
       );
     },
-    maxMP: function() {
+    currentMP: function() {
       return this.status_list[2].value;
     },
-    SAN: function() {
+    currentSAN: function() {
       return this.status_list[2].value * 5;
-    }
+    },
 
     // 技能
-    // もし79以上なら，EDU21のとき99を超えるため除外
-    // もし82以上なら．INT18のとき99を超えるため除外
+    ability_list: function() {
+      let abilities = [
+        {
+          name: "言いくるめ",
+          value: 5
+        },
+        {
+          name: "医学",
+          value: 5
+        },
+        {
+          name: "運転（自動車）",
+          value: 20
+        },
+        {
+          name: "応急手当",
+          value: 30
+        },
+        {
+          name: "オカルト",
+          value: 5
+        },
+        {
+          name: "回避",
+          value: this.status_list[3].value * 2
+        },
+        {
+          name: "化学",
+          value: 1
+        },
+        {
+          name: "鍵開け",
+          value: 1
+        },
+        {
+          name: "隠す",
+          value: 15
+        },
+        {
+          name: "隠れる",
+          value: 10
+        },
+        {
+          name: "機械修理",
+          value: 20
+        },
+        {
+          name: "聞き耳",
+          value: 25
+        },
+        {
+          name: "芸術",
+          value: 5
+        },
+        {
+          name: "経理",
+          value: 10
+        },
+        {
+          name: "考古学",
+          value: 1
+        },
+        {
+          name: "コンピュータ",
+          value: 1
+        },
+        {
+          name: "忍び歩き",
+          value: 10
+        },
+        {
+          name: "写真術",
+          value: 10
+        },
+        {
+          name: "重機械操作",
+          value: 1
+        },
+        {
+          name: "乗馬",
+          value: 5
+        },
+        {
+          name: "信用",
+          value: 15
+        },
+        {
+          name: "心理学",
+          value: 5
+        },
+        {
+          name: "人類学",
+          value: 1
+        },
+        {
+          name: "水泳",
+          value: 25
+        },
+        {
+          name: "製作",
+          value: 5
+        },
+        {
+          name: "精神分析",
+          value: 1
+        },
+        {
+          name: "生物学",
+          value: 1
+        },
+        {
+          name: "説得",
+          value: 15
+        },
+        {
+          name: "操縦",
+          value: 1
+        },
+        {
+          name: "地質学",
+          value: 1
+        },
+        {
+          name: "跳躍",
+          value: 25
+        },
+        {
+          name: "追跡",
+          value: 10
+        },
+        {
+          name: "電気修理",
+          value: 10
+        },
+        {
+          name: "電子工学",
+          value: 1
+        },
+        {
+          name: "天文学",
+          value: 1
+        },
+        {
+          name: "投擲",
+          value: 25
+        },
+        {
+          name: "登攀",
+          value: 40
+        },
+        {
+          name: "図書館",
+          value: 25
+        },
+        {
+          name: "ナビゲート",
+          value: 10
+        },
+        {
+          name: "値切り",
+          value: 5
+        },
+        {
+          name: "博物学",
+          value: 10
+        },
+        {
+          name: "物理学",
+          value: 1
+        },
+        {
+          name: "変装",
+          value: 1
+        },
+        {
+          name: "法律",
+          value: 5
+        },
+        {
+          name: "ほかの言語",
+          value: 1
+        },
+        {
+          name: "母国語",
+          value: this.status_list[7].value * 5
+        },
+        {
+          name: "マーシャルアーツ",
+          value: 1
+        },
+        {
+          name: "目星",
+          value: 25
+        },
+        {
+          name: "薬学",
+          value: 1
+        },
+        {
+          name: "歴史",
+          value: 20
+        },
+        {
+          name: "拳銃",
+          value: 20
+        },
+        {
+          name: "キック",
+          value: 25
+        },
+        {
+          name: "組みつき",
+          value: 25
+        },
+        {
+          name: "こぶし",
+          value: 50
+        },
+        {
+          name: "頭突き",
+          value: 10
+        }
+      ]
+
+      // 職業技能割り振り
+      const eduP = this.status_list[7].value
+      for (let i = 0; i < 20; i++) {
+        let tmpIndex = Math.floor(Math.random() * 17)
+        // もし79以上なら，EDU21のとき99を超えるため除外
+        if (abilities[tmpIndex].value > 78) {
+          i--
+        } else {
+          abilities[tmpIndex].value += eduP
+        }
+      }
+
+      // 興味技能割り振り
+      const intP = this.status_list[6].value
+      for (let i = 0; i < 10; i++) {
+        let tmpIndex = Math.floor(Math.random() * 17)
+        // もし82以上なら．INT18のとき99を超えるため除外
+        if (abilities[tmpIndex].value > 82) {
+          i--
+        } else {
+          abilities[tmpIndex].value += intP
+        }
+      }
+
+      return abilities
+    }
     // 探偵：職業技能「言いくるめ，鍵開け，心理学，追跡，図書館，法律，目星，（1：聞き耳，写真術，値切り，こぶし／パンチ）」
   },
 
   methods: {
-    // URLを発行する
     get2D6() {
       return Math.floor(Math.random() * 6) + Math.floor(Math.random() * 6);
     },
