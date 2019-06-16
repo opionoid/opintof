82ff8a26b7387d8df403a3c7dad36731ee4433ef
Opion <kindledkindred@gmail.com>
2019-06-16 16:05:53 +0900

    create bone of CoCSelectJob.vue

---
+++ src/views/CoCSelectJob.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  v-container
+    v-layout(row wrap)
+      v-flex(xs3 v-for="job in job_list")
+        v-card(@click="router.push('coc_character')")
+          v-img(src="job.icon")
+          v-card-title(primary-title)
+            h1.headline {{ job.name }}
+</template>
